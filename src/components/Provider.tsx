cd603927fde2a9f1c55c9a1fd5c64202a22c9479
kdh379 <ehgus37625@naver.com>
2024-02-13 15:54:04 +0900

    feat: toast

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -6,9 +6,10 @@ import { cn } from "@/lib/utils";
 import { ClientSideProvider } from "./ClientSideProvider";
 import AppHeader from "./layout/AppHeader";
 import Sidebar from "./layout/Sidebar";
+import { Toaster } from "./ui/toaster";
+
 
 export default function Providers(props: PropsWithChildren) {
-
   const postList = getAllPosts();
 
   return (
@@ -17,14 +18,15 @@ export default function Providers(props: PropsWithChildren) {
       defaultTheme="system"
       enableSystem
     >
-      <div className="bg-background relative flex min-h-screen flex-col transition ease-in-out">
+      <div className="bg-background relative min-h-screen antialiased transition ease-in-out">
         <AppHeader postList={postList} />
         <div className={cn(
-          "desktop:grid desktop:grid-cols-[240px_minmax(0,1fr)] desktop:gap-10 container relative min-h-screen flex-1 antialiased"
+          "tablet:grid tablet:grid-cols-[240px_minmax(0,1fr)] tablet:gap-10 container min-h-screen flex-1 antialiased"
         )}>
           <Sidebar postList={postList} />
           {props.children}
         </div>
+        <Toaster />
       </div>
     </ClientSideProvider>
   );
--- src/components/ui/copy-button.tsx
+++ src/components/ui/copy-button.tsx
@@ -1,23 +1,38 @@
 "use client";
 
 import { CheckIcon, CopyIcon } from "@radix-ui/react-icons";
+import { CheckCircle } from "lucide-react";
 import React from "react";
 
 import { cn } from "@/lib/utils";
 
 import { Button } from "./button";
+import { useToast } from "./use-toast";
 
 interface CopyButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   value: string;
 };
 
+const CopyMessage = () => (
+  <div className="flex items-center space-x-2">
+    <CheckCircle className="text-primary size-4" />
+    <span>복사되었습니다.</span>
+  </div>
+);
+
 export default function CopyButton( props: CopyButtonProps ) {
   const [copied, setCopied] = React.useState(false);
+  const { toast, dismiss } = useToast();
 
   const copyToClipboard = async () => {
     await navigator.clipboard.writeText(props.value);
+    toast({
+      title: <CopyMessage />,
+    });
     setCopied(true);
-    setTimeout(() => setCopied(false), 2000);
+    setTimeout(() => {
+      dismiss();
+      setCopied(false);}, 2000);
   }; 
 
   return (
