4542f4b22a398887a431d34ca0c945e01628c6ab
kdh379 <ehgus37625@naver.com>
2024-02-20 22:35:06 +0900

    fix(linked-card).:  mobile 환경에서 og image 찌그러짐

--- src/components/ui/linked-card.tsx
+++ src/components/ui/linked-card.tsx
@@ -45,25 +45,27 @@ export default function LinkedCard({ href, title }: LinkCardProps) {
       rel="noopener noreferrer"
       className="bg-muted/50 hover:bg-muted/100 mobile:h-32 mb-4 flex h-24 rounded-md border transition-colors active:opacity-50"
     >
-      {isLoading && <div className="bg-accent h-full animate-pulse" />}
-      {openGraph?.ogImage?.length && (
-        <div
-          style={{
-            backgroundImage: `url(${openGraph?.ogImage[0].url})`,
-            aspectRatio: "1 / 1",
-          }} 
-          className="mobile:w-32 left-0 top-0 w-24 rounded-l-md bg-cover bg-center" />
-      )}
-      <div className="mobile:p-4 flex flex-col overflow-hidden p-2">
-        <p className="w-full overflow-hidden text-ellipsis text-nowrap text-lg font-medium">
-          {openGraph?.ogTitle}
-        </p>
-        <p className="text-muted-foreground mobile:line-clamp-2 line-clamp-1 overflow-hidden text-ellipsis text-sm">
-          {openGraph?.ogDescription}
-        </p>
-        <p className="text-primary mt-auto text-sm font-medium underline underline-offset-4">
-          {href}
-        </p>
+      <div className="flex overflow-hidden">
+        {isLoading && <div className="bg-accent h-full animate-pulse" />}
+        {openGraph?.ogImage?.length && (
+          <div
+            style={{
+              backgroundImage: `url(${openGraph?.ogImage[0].url})`,
+              aspectRatio: "1/1",
+            }} 
+            className="mobile:w-32 left-0 top-0 w-24 rounded-l-md bg-cover bg-center" />
+        )}
+        <div className="mobile:p-4 flex flex-1 flex-col overflow-hidden p-2">
+          <p className="line-clamp-1 text-lg font-medium">
+            {openGraph?.ogTitle}
+          </p>
+          <p className="text-muted-foreground mobile:line-clamp-2 line-clamp-1 text-sm">
+            {openGraph?.ogDescription}
+          </p>
+          <p className="text-primary mt-auto line-clamp-1 text-sm font-medium underline underline-offset-4">
+            {href}
+          </p>
+        </div>
       </div>
     </a>
   </>;
