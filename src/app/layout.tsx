8dad5a3a6acecb4d378fd2b12588413ba05d08e1
kdh379 <ehgus37625@naver.com>
2024-02-28 00:37:45 +0900

    chore: change font subset

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,22 +55,22 @@ export const viewport: Viewport = {
 const pretendard = localFont({
   src: [
     {
-      path: "../../public/fonts/Pretendard-Regular.woff2",
+      path: "../../public/fonts/Pretendard-Regular.subset.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../../public/fonts/Pretendard-Medium.woff2",
+      path: "../../public/fonts/Pretendard-Medium.subset.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../../public/fonts/Pretendard-SemiBold.woff2",
+      path: "../../public/fonts/Pretendard-SemiBold.subset.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../../public/fonts/Pretendard-Bold.woff2",
+      path: "../../public/fonts/Pretendard-Bold.subset.woff2",
       weight: "700",
       style: "normal",
     },
