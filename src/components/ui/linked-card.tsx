468680bc3768eaefe522ff5cc9fa0974558006f7
kdh379 <ehgus37625@naver.com>
2024-03-05 23:11:11 +0900

    chore: og-card suspense

--- src/components/ui/linked-card.tsx
+++ src/components/ui/linked-card.tsx
@@ -1,4 +1,7 @@
 
+import { ImageIcon } from "lucide-react";
+import React from "react";
+
 import api from "@/lib/api";
 
 import { Button } from "./button";
@@ -8,15 +11,7 @@ interface LinkCardProps {
   title?: string;
 }
 
-export default async function LinkedCard({ href, title }: LinkCardProps) {
-
-  const openGraph = await api({
-    key: "getOpenGraph",
-    params: { url: href },
-    next: {
-      revalidate: 1000 * 60 * 60 * 24,
-    },
-  });
+export default function LinkedCard({href, title}: LinkCardProps) {
 
   return <>
     <Button
@@ -32,33 +27,60 @@ export default async function LinkedCard({ href, title }: LinkCardProps) {
         {title || href}
       </a>
     </Button>
-    <a
-      href={href}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="bg-muted/50 hover:bg-muted/100 mobile:h-32 mb-4 flex h-24 rounded-md border transition-colors active:opacity-50"
-    >
-      <div className="flex overflow-hidden">
-        {openGraph?.ogImage?.length && (
-          <div
-            style={{
-              backgroundImage: `url(${openGraph?.ogImage[0].url})`,
-              aspectRatio: "1/1",
-            }} 
-            className="mobile:w-32 left-0 top-0 w-24 rounded-l-md bg-cover bg-center" />
-        )}
-        <div className="mobile:p-4 flex flex-1 flex-col overflow-hidden p-2">
-          <p className="line-clamp-1 text-lg font-medium">
-            {openGraph?.ogTitle}
-          </p>
-          <p className="text-muted-foreground mobile:line-clamp-2 line-clamp-1 text-sm">
-            {openGraph?.ogDescription}
-          </p>
-          <p className="text-primary mt-auto line-clamp-1 text-sm font-medium underline underline-offset-4">
-            {href}
-          </p>
+    <React.Suspense
+      fallback={
+        <div className="bg-muted/50 mobile:h-32 mb-4 flex animate-pulse rounded-md border">
+          <div className="mobile:w-32 bg-muted flex w-24 items-center justify-center rounded-l-md">
+            <ImageIcon />
+          </div>
+          <div className="mobile:p-4 flex flex-1 flex-col p-2">
+            <div className="bg-muted/100 h-4 w-3/4 rounded" />
+            <div className="bg-muted/100 mt-2 h-4 w-full rounded" />
+            <div className="bg-muted/100 mt-auto h-4 w-3/4 rounded" />
+          </div>
         </div>
-      </div>
-    </a>
+      }
+    >
+      <Component href={href} />
+    </React.Suspense>
   </>;
+}
+
+async function Component({ href }: LinkCardProps) {
+  const openGraph = await api({
+    key: "getOpenGraph",
+    params: { url: href },
+    next: {
+      revalidate: 1000 * 60 * 60 * 24,
+    },
+  });
+
+  return <a
+    href={href}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="bg-muted/50 hover:bg-muted/100 mobile:h-32 mb-4 flex h-24 rounded-md border transition-colors active:opacity-50"
+  >
+    <div className="flex overflow-hidden">
+      {openGraph?.ogImage?.length && (
+        <div
+          style={{
+            backgroundImage: `url(${openGraph?.ogImage[0].url})`,
+            aspectRatio: "1/1",
+          }} 
+          className="mobile:w-32 left-0 top-0 w-24 rounded-l-md bg-[length:8rem_100%] bg-no-repeat"/>
+      )}
+      <div className="mobile:p-4 flex flex-1 flex-col overflow-hidden p-2">
+        <p className="line-clamp-1 text-lg font-medium">
+          {openGraph?.ogTitle}
+        </p>
+        <p className="text-muted-foreground mobile:line-clamp-2 line-clamp-1 text-sm">
+          {openGraph?.ogDescription}
+        </p>
+        <p className="text-primary mt-auto line-clamp-1 text-sm font-medium underline underline-offset-4">
+          {href}
+        </p>
+      </div>
+    </div>
+  </a>;
 }
--- src/components/ui/video-card.tsx
+++ src/components/ui/video-card.tsx
@@ -1,4 +1,7 @@
 
+import { VideoIcon } from "lucide-react";
+import * as React from "react";
+
 import api from "@/lib/api";
 import { cn } from "@/lib/utils";
 
@@ -27,15 +30,31 @@ export default async function VideoCard({ href }: VideoCardProps) {
       </p>
     </a>
     <div className="bg-muted/50 relative aspect-video rounded-md pb-[56.25%]">
-      {openGraph?.ogVideo.length &&
-        <iframe
-          title={openGraph?.ogTitle || "Video"}
-          src={openGraph?.ogVideo?.[0]?.url}
-          className="absolute inset-0 size-full rounded-md"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        />
-      }
+      <React.Suspense 
+        fallback={
+          <div>
+            <VideoIcon className="text-muted size-8" />
+          </div>
+        }
+      >
+        <Component href={href} />
+      </React.Suspense>
     </div>
   </div>;
 }
+
+async function Component({ href }: VideoCardProps) {
+  const openGraph = await api({
+    key: "getOpenGraph",
+    params: { url: href },
+  });
+
+  return openGraph?.ogVideo.length &&
+    <iframe
+      title={openGraph?.ogTitle || "Video"}
+      src={openGraph?.ogVideo?.[0]?.url}
+      className="absolute inset-0 size-full rounded-md"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    />;
+};
