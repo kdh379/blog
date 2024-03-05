5662129c5ca72a329a2e86f675d7a5611eac654f
kdh379 <kdh379@innorules.com>
2024-03-05 15:40:51 +0900

    chore: robots sitemap 수정

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: [],
     },
-    sitemap: "/sitemap.xml",
+    sitemap: "https://blog.bandal.dev/sitemap.xml",
   };
 }
