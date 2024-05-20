0914fb8f92433f91b516f0d891b3bd85fac16b1b
kdh379 <ehgus37625@naver.com>
2024-05-21 03:20:51 +0900

    chore: spotlight 색상 변경

--- apps/blog/src/components/layout/Spotlights.tsx
+++ apps/blog/src/components/layout/Spotlights.tsx
@@ -2,7 +2,7 @@ import { Spotlight } from "@repo/ui/components/ui/spotlight";
 
 function Spotlights() {
   return (
-    <div>
+    <div className="absolute w-full h-screen overflow-hidden">
       <Spotlight
         className="h-screen -top-40 -left-10 md:-left-32 md:-top-20"
         fill="white"
@@ -11,7 +11,7 @@ function Spotlights() {
         className="h-[80vh] w-[50vw] top-10 left-full"
         fill="purple"
       />
-      <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="hsla(var(--primary) / 0.5)" />
     </div>
   );
 }
