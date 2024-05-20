889fede3d8bff82ecc7aeb177ece1e0824048828
kdh379 <ehgus37625@naver.com>
2024-05-21 02:18:54 +0900

    chore: apps\web 제거

--- apps/web/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: [
-    "@repo/eslint-config/next.js",
-    "@repo/eslint-config/rules.js"
-  ],
-  parser: "@typescript-eslint/parser",
-};
