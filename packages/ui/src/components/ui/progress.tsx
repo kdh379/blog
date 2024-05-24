d0e90f50b7d502e1f56809662552c159cfd0a403
kdh379 <ehgus37625@naver.com>
2024-05-24 12:23:16 +0900

    feat(ui): progress, skeleton

---
+++ packages/ui/src/components/ui/progress.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import * as ProgressPrimitive from "@radix-ui/react-progress";
+import { cn } from "@ui/lib/utils";
+import * as React from "react";
+
+const Progress = React.forwardRef<
+  React.ElementRef<typeof ProgressPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & { value?: number }
+>(({ className, value, ...props }, ref) => (
+  <ProgressPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative h-4 w-full overflow-hidden rounded-full bg-secondary",
+      className
+    )}
+    {...props}
+  >
+    <ProgressPrimitive.Indicator
+      className="flex-1 w-full h-full transition-all bg-primary"
+      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+    />
+  </ProgressPrimitive.Root>
+));
+Progress.displayName = ProgressPrimitive.Root.displayName;
+
+export { Progress };
