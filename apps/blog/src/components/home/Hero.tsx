020bc597f9fc85ab3c124971b7c37165a13673c2
kdh379 <ehgus37625@naver.com>
2024-06-06 01:20:25 +0900

    feat: hero
    - 메인 페이지 hero에 reveal 애니메이션 추가

--- apps/blog/src/components/home/Hero.tsx
+++ apps/blog/src/components/home/Hero.tsx
@@ -1,9 +1,10 @@
+import { Link } from "@repo/ui/components/Link";
 import { buttonVariants } from "@repo/ui/components/ui/button";
 import { Icons } from "@repo/ui/components/ui/icons";
+import Reveal from "@repo/ui/components/ui/reveal";
 import TypeWriter from "@repo/ui/components/ui/type-writer";
 import { cn } from "@repo/ui/lib/utils";
 import { siteConfig } from "@repo/ui/site.config";
-import Link from "next/link";
 
 const skills = [
   "React",
@@ -33,14 +34,18 @@ function Anchor({ href, children }: { href: string; children: React.ReactNode })
 function Hero() {
   return (
     <section className="my-8 slide-enter-content">
-      <h1 className="mb-4 text-2xl">프론트엔드 개발자 <span className="font-bold text-primary">김도현</span>입니다.</h1>
+      <Reveal>
+        <h1 className="text-2xl">프론트엔드 개발자 <span className="font-bold text-primary">김도현</span>입니다.</h1>
+      </Reveal>
 
-      <p>웹 개발과 저에 대해 작성하고 있습니다.</p>
-      <p>보고 배운걸 까먹지 않기 위해 기록해둔 곳입니다.</p>
-      <p>동료에게 좋은 평가를 받는 것을 좋아합니다. 그래서 더 노력합니다.</p>
-      <p>프론트엔드 개발 시 주로 <TypeWriter strings={skills} />를 사용합니다.</p>
+      <Reveal className="mt-4">
+        <p>웹 개발과 저에 대해 작성하고 있습니다.</p>
+        <p>보고 배운걸 까먹지 않기 위해 기록해둔 곳입니다.</p>
+        <p>동료에게 좋은 평가를 받는 것을 좋아합니다. 그래서 더 노력합니다.</p>
+        <p>프론트엔드 개발 시 주로 <TypeWriter strings={skills} />를 사용합니다.</p>
+      </Reveal>
 
-      <div className="mt-12">
+      <Reveal className="mt-12">
         <p className="mb-2">Connect me on</p>
         <div className="flex flex-wrap items-center gap-4">
           <Link 
@@ -63,7 +68,7 @@ function Hero() {
             이전 블로그
           </Anchor>
         </div>
-      </div>
+      </Reveal>
     </section>
   );
 }
---
+++ packages/ui/src/components/ui/reveal.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { cn } from "@ui/lib/utils";
+import { motion } from "framer-motion";
+import { useRef } from "react";
+
+interface RevealProps {
+  children: React.ReactNode;
+  width?: "fit-content" | "100%";
+  className?: string;
+}
+
+export default function Reveal({width = "fit-content", className, children}: RevealProps) {
+  
+  const ref = useRef<HTMLDivElement>(null);
+
+  return (
+    <div ref={ref} className={cn("relative", width, className)}>
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 0.5, delay: 0.25 }}
+      >
+        {children}
+      </motion.div>
+      <motion.div
+        variants={{
+          hidden: { left: 0 },
+          visible: { left: "100%" },
+        }}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 0.5, ease: "easeIn" }}
+        className="absolute top-0 bottom-0 left-0 right-0 z-10 bg-primary"
+      />
+    </div>
+  );
+}
