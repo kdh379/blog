52599b81da8cb736c6b3ed4851184fcf59424518
kdh379 <ehgus37625@naver.com>
2024-06-26 22:41:11 +0900

    feat: post sort
    - 카테고리, 날짜순 정렬

--- apps/blog/src/lib/post.ts
+++ apps/blog/src/lib/post.ts
@@ -3,6 +3,7 @@ import { allPosts, type Post } from "contentlayer/generated";
 interface Item {
   category?: string;
   title?: string;
+  date?: string;
   slugAsParams?: string;
   children?: Item[];
 }
@@ -32,16 +33,54 @@ function createTree(posts: Post[]) {
       currentNode = foundNode;
     }
 
+
     currentNode.title = post.title;
-    currentNode.category = undefined;
+    currentNode.date = post.date;
     currentNode.slugAsParams = `/${post.slugAsParams}`;
   });
 
   return root.children || [];
 }
 
+const SORT_CATEGORY = ["회고", "React", "javascript", "web"];
+
+function sortTree(tree: Item[]) {
+  tree.sort((a, b) => {
+    // SORT_CATEGORY 순서대로 카테고리 정렬
+    if( !a.category || !b.category ) return 0;
+
+    const aIndex = SORT_CATEGORY.indexOf(a.category);
+    const bIndex = SORT_CATEGORY.indexOf(b.category);
+
+    if( aIndex !== -1 && bIndex !== -1 ) {
+      return aIndex - bIndex;
+    }
+
+    if( aIndex !== -1 ) {
+      return -1;
+    }
+
+    if( bIndex !== -1 ) {
+      return 1;
+    }
+
+    if( !a.date || !b.date) return 0;
+
+    // category가 같으면 날짜순으로 정렬
+    return (b.date || "").localeCompare(a.date || "");
+  });
+
+  tree.forEach((node) => {
+    if( node.children ) {
+      sortTree(node.children);
+    }
+  });
+
+  return tree;
+}
+
 export function getPostTree(): PostTree {
   const tree = createTree(allPosts);
 
-  return tree;
+  return sortTree(tree);
 }
