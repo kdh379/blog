31bdaf54cf1c57ea761dd96451e35c6efaed0215
kdh379 <ehgus37625@naver.com>
2024-06-26 23:10:11 +0900

    chore: error-boundary sentry
    - error fallback 시 sentry에 에러 메시지 전달

--- packages/ui/src/components/template/fallback-retry.tsx
+++ packages/ui/src/components/template/fallback-retry.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import * as Sentry from "@sentry/nextjs";
+import { RefreshCcw } from "lucide-react";
+
 import { Button } from "@ui/components/ui/button";
 
 interface ErrorFallbackProps {
@@ -11,15 +14,38 @@ interface ErrorFallbackProps {
 }
 
 export  default function FallbackRetry({ error, reset }: ErrorFallbackProps) {
+  // sentry에 에러 전송
+  if (error)
+    Sentry.captureException(new Error(`Error: ${error.status} - ${error.message}`));
+
   return (
     <div>
-      <h1 className="headline mt-8 pb-4 text-3xl md:text-4xl lg:text-5xl">
-        {error ? `Error: ${error.status}` : "알 수 없는 오류가 발생했습니다."}
+      <h1 className="mt-8 pb-4 text-2xl font-bold leading-5">
+        서비스 요청 중 오류가 발생했습니다.
       </h1>
       <p className="text-lg">
-        {error ? `Error code: ${error.message}` : "다시 시도해주세요."}
+        잠시 후 다시 시도해주세요.
       </p>
-      {reset && <Button onClick={reset}>다시 시도</Button>}
+      {error && (
+        <>
+          <p className="mt-4 text-lg">
+            {error.status}
+          </p>
+          <p className="text-muted-foreground leading-5">
+            {error.message}
+          </p>
+        </>
+      )}
+      {reset && (
+        <Button
+          onClick={reset}
+          size="lg"
+          className="mt-4"
+        >
+          <RefreshCcw size={16} className="mr-2" />
+          다시 시도
+        </Button>
+      )}
     </div>
   );
 }
